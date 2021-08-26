650bf6d0359bb07b4453f3605ac6c841b5ac684d
hgxey6 <hgxey6@gmail.com>
2021-08-26 15:29:43 +0500

    bug fix

--- src/main/java/com/github/hgxey6/weather/controller/WeatherController.java
+++ src/main/java/com/github/hgxey6/weather/controller/WeatherController.java
@@ -2,13 +2,17 @@ package com.github.hgxey6.weather.controller;
 
 import com.github.hgxey6.weather.model.Weather;
 import com.github.hgxey6.weather.service.CurrentWeatherService;
+import com.github.hgxey6.weather.service.WeatherDataService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class WeatherController {
 
     private CurrentWeatherService currentWeatherService;
+    private WeatherDataService weatherDataService;
 
     public WeatherController(CurrentWeatherService currentWeatherService) {
         this.currentWeatherService = currentWeatherService;
@@ -16,7 +20,11 @@ public class WeatherController {
 
     @GetMapping("/current-weather")
     public Weather getCurrentWeather() {
-        Weather weather = currentWeatherService.getCurrentWeather();
-        return weather;
+        return currentWeatherService.getCurrentWeather();
+    }
+
+    @GetMapping("/weather")
+    public List<Weather> getAll() {
+        return weatherDataService.getAll();
     }
 }
